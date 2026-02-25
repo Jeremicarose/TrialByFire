65838fdde03f2f22721ff2e24f3310d4cbece97e
Jeremicarose <jeremicarose@example.com>
2026-02-25 17:02:02 +0300

    feat(contracts): update TrialMarket contract

--- packages/contracts/contracts/TrialMarket.sol
+++ packages/contracts/contracts/TrialMarket.sol
@@ -59,5 +59,6 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
  * Lifecycle:
  *  1. createMarket()               - Anyone posts a question + 0.01 ETH deposit
  *  2. takePosition()               - Stake ETH on YES or NO
- *  3. requestSettlement()          - After deadline(manual or via )
+ *  3. requestSettlement()          - After deadline(manual or via Automation)
+ *  4. semdTrialRequest()           - Triggers Chainlink 
  */
