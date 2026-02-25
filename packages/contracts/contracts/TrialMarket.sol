66e5d3ca08704a14a8618bd39742ec356b0d4de7
Jeremicarose <jeremicarose@example.com>
2026-02-25 16:51:39 +0300

    feat(contracts): update TrialMarket contract

--- packages/contracts/contracts/TrialMarket.sol
+++ packages/contracts/contracts/TrialMarket.sol
@@ -41,5 +41,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
  * Architecture:
  *      This contract integrates THree Chainlink technologies:
  * 
- *      1. Chainlink Functions - Executes the adversarial trial off-chai
+ *      1. Chainlink Functions - Executes the adversarial trial off-chain on
+ *          Chainlink's Decentralized Orcle Network (DON). The trial JavaScript
+ *          source
  */
