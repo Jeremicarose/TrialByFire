334bfc511800f791aad42333f616e7892d776b87
Jeremicarose <jeremicarose@example.com>
2026-02-25 20:29:35 +0300

    feat(contracts): update TrialMarket contract

--- packages/contracts/contracts/TrialMarket.sol
+++ packages/contracts/contracts/TrialMarket.sol
@@ -364,6 +364,6 @@ contract TrialMarket is Ownable, ReentracyGuard, FunctionsClient, AutomationComp
     function requestSettlement(uint256 marketId) public {
         Market storage m = market[marketId];
         require(m.status == MarketStatus.Open, "Market not open");
-        require(block.timestamp >= m.deadline)
+        require(block.timestamp >= m.deadline,)
     }
 }
